110ed327140b20bb954d16dd2517b5c816e5ba83
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-07-12 14:27:08 +1000

    creat a class

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,5 +1,5 @@
-export default class ClassRoom{
-  constructor(maxStudentSize) {
-    this._maxStudentSize = maxStudentSize;
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+      this._maxStudentsSize = maxStudentsSize;
   }
 }
