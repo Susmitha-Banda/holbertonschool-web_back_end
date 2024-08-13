173583212fbcb73bfee4d8546b0966ab5221ced1
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-08-13 16:20:14 +1000

    returns the string representation of the float

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''This module takes a float n as argument and
+returns the string representation of the float.'''
+
+
+def to_str(n: float) -> str:
+    '''Type-annoted function to_str
+    Arguments: n(float)
+    returns the floor of the float'''
+    return str(n)
