17e438bab2054a967331c6e5c208c837b144441c
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-08-13 17:06:26 +1000

    function that multiplies

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+''' takes a float multiplier as argument and returns a
+function that multiplies a float by multiplier'''
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''
+    function 'make_multiplier'
+    Args: float
+    Return: function that multiplies
+    '''
+    def multiplier_func(x: float) -> float:
+      return (x * multiplier)
+    return multiplier_func
