a8b8ca244fac240249863ac9d005c3d00eb87545
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-08-13 16:31:06 +1000

    defines and annotates the variables with specified values

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -6,5 +6,5 @@ returns the string representation of the float.'''
 def to_str(n: float) -> str:
     '''Type-annoted function to_str
     Arguments: n(float)
-    returns the floor of the float'''
+    returns the string of the float'''
     return str(n)
