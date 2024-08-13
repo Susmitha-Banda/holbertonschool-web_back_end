7e1267dbfdf994188056f8f1807e3ec3dd6d2651
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-08-13 16:05:35 +1000

    concatination of two strings

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''This module takes a string str1 and a string str2
+as arguments and returns a concatenated string'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''Type-annoted function concatinate
+    Arguments: str1(string), str2(string)
+    returns the concatinated string of str1 and str2'''
+    return (str1 + str2)
