b68bea827f73320b9dccdecaee24d9df2a50170a
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-08-13 15:59:46 +1000

    pycodestyle error rectified

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-'''This module takes a float a and a float b as arguments and returns their sum as a float'''
+'''This module takes a float a and a float b as
+  arguments and returns their sum as a float'''
 
 
 def add(a: float, b: float) -> float:
-  '''Type-annoted function add
-  Arguments a: float,b: float
-  return the sum of a and b'''
-  return a + b
-    
+    '''Type-annoted function add
+    Arguments a: float,b: float
+    returns the sum of a and b'''
+    return a + b
