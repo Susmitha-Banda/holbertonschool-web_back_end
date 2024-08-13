ee1cede068c57be8772271a2c6191b274e8af003
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-08-13 15:56:34 +1000

    a variable annotation of sum of 2 floats

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''This module takes a float a and a float b as arguments and returns their sum as a float'''
+
+
+def add(a: float, b: float) -> float:
+  '''Type-annoted function add
+  Arguments a: float,b: float
+  return the sum of a and b'''
+  return a + b
+    
