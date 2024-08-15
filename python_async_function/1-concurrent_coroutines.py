7d1af627b2d71caecc1551e7fe9cf45832d1469a
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-08-15 15:19:47 +1000

    pycode error

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,6 +6,7 @@ from typing import List
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     '''
     Function: async routine 'wait_n'
