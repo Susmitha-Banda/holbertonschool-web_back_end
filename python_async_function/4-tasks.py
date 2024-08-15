3d63f43b2271f19783c0e474a4b5405ca3aedd5a
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-08-15 15:51:37 +1000

    async routine 'task_wait_n'

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
-'''
-A module that takes two integers and returns a list of all delays
-'''
-from typing import List
+"""Nameless module for Python Async Task 1"""
 import asyncio
+from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    '''
-    Function: async routine 'task_wait_n'
-    Arguments: n: int, max_delay: int
-    Return: list of all the delays (float values)
-    '''
-    # Step 1: Create a list of coroutines
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    # Step 2: Use asyncio.gather to run them concurrently
-    delays = await asyncio.gather(*tasks)
-    # Step 3: Sort and return the list of delays
-    return (delays)
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """calls wait_random n number of times"""
+
+    delays = []
+    tasks = []
+    for x in range(n):
+        tasks.append(task_wait_random(max_delay))
+
+    for task in asyncio.as_completed(tasks):
+        result = await task
+        delays.append(result)
+
+    return delays
