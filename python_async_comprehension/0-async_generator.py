e08f1822c97eb1e42fa7916edf716105b1cb4227
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-08-19 14:39:53 +1000

    annotation revised

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 '''coroutine called async_generator that takes no arguments'''
 import random
+from typing import Generator
 import asyncio
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     for _ in range(10):
         # Asynchronously wait for 1 second
         await asyncio.sleep(1)
