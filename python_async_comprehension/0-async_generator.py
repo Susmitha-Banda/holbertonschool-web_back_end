a1c45ee7ece1f7c8e3608ee4220bd34df697c5c9
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-08-19 14:41:39 +1000

    documentation updated

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,6 +6,8 @@ import asyncio
 
 
 async def async_generator() -> Generator[float, None, None]:
+    '''function: async_generator, arguments:none,
+      return: random numbers bw 1-10 '''
     for _ in range(10):
         # Asynchronously wait for 1 second
         await asyncio.sleep(1)
