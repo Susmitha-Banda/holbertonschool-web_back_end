8340122b1bcaf74211dd52f348b7f2671b9300ef
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-09-10 14:05:11 +1000

    task 2

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,31 +1,8 @@
 const fs = require('fs');
 
-module.exports = function countStudents(filename) {
+module.exports = function countStudents(path) {
   try {
-    // const data = fs.readFileSync(filename, 'utf-8');
-    // const rows = data.split('\n');
-    // let rowCount = 0;
-    // const sweNames = [];
-    // const csNames = [];
-
-    // for (const row of rows) {
-    //   rowCount += 1;
-
-    //   if (row.substring(row.length - 3) === 'SWE') {
-    //     sweNames.push(row.split(',')[0]);
-    //   }
-    //   if (row.substring(row.length - 2) === 'CS') {
-    //     csNames.push(row.split(',')[0]);
-    //   }
-    // }
-
-    // console.log(`Number of students: ${rowCount - 1}`);
-    // console.log(`Number of students in CS: ${csNames.length}. List: ${csNames.join(', ')}`);
-    // console.log(`Number of students in SWE: ${sweNames.length}. List: ${sweNames.join(', ')}`);
-
-    // ----- Old code that doesn't pass checker ends here ----
-
-    const data = fs.readFileSync(filename, 'utf-8');
+    const data = fs.readFileSync(path, 'utf8');
     const rows = data.split('\n').slice(1);
 
     const studentsCS = [];
@@ -34,11 +11,12 @@ module.exports = function countStudents(filename) {
     for (const row of rows) {
       const data = row.split(',');
 
-      if (data[3] === 'CS') { // hardcode
+      // if field is CS add to CS array
+      if (data[3] === 'CS') {
         studentsCS.push(data[0]);
       }
-
-      if (data[3] === 'SWE') { // hardcode
+      // if field is SWE add to SWE array
+      if (data[3] === 'SWE') {
         studentsSWE.push(data[0]);
       }
     }
@@ -46,7 +24,7 @@ module.exports = function countStudents(filename) {
     console.log(`Number of students: ${studentsCS.length + studentsSWE.length}`);
     console.log(`Number of students in CS: ${studentsCS.length}. List: ${studentsCS.join(', ')}`);
     console.log(`Number of students in SWE: ${studentsSWE.length}. List: ${studentsSWE.join(', ')}`);
-  } catch (error) {
+  } catch (err) {
     throw new Error('Cannot load the database');
   }
 };
